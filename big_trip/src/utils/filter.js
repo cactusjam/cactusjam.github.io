1fdd89dad06788089bee4cd71222643856b2888b
cactusjam <58633055+cactusjam@users.noreply.github.com>
2020-10-05 18:43:45 +0300

    Delete filter.js

--- big_trip/src/utils/filter.js
+++
@@ -1,9 +0,0 @@
-import {FilterType} from "../constants";
-
-const filterTypeToPoints = {
-  [FilterType.EVERYTHING]: (points) => points.slice(),
-  [FilterType.FUTURE]: (points, now) => points.filter((point) => point.startDate > now),
-  [FilterType.PAST]: (points, now) => points.filter(({startDate}) => startDate < now)
-};
-
-export {filterTypeToPoints};
