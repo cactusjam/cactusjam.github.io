e9931ada724c64373e812a92bfc3dd99c92a6bae
cactusjam <58633055+cactusjam@users.noreply.github.com>
2020-10-05 18:43:01 +0300

    Delete dom-event.js

--- big_trip/src/utils/dom-event.js
+++
@@ -1,7 +0,0 @@
-import {KeyboardKey} from "../constants.js";
-
-const isEscapeEvent = (evt) => {
-  return (evt.key === KeyboardKey.ESCAPE || evt.key === KeyboardKey.ESCAPE_IE);
-};
-
-export {isEscapeEvent};
